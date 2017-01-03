9f8c7f59f8025a0d38c2957d9186d61b22152321
jon <zhijiang.wang@busonline.com>
2017-01-03 20:02:38 +0800

    ccc

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -13,6 +16,11 @@ public class DemoApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+	    //System.setProperty("","");
+	    SpringApplication app = new SpringApplication(DemoApplication.class);
+	    app.setBannerMode(Banner.Mode.OFF);
+	    app.run(args);
+		//SpringApplication.run(DemoApplication.class, args);
 	}
 }
