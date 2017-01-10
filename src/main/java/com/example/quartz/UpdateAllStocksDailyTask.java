25191ccd4cf1c8c84954ded565c4f1f84d7c873b
jon <zhijiang.wang@busonline.com>
2017-01-10 21:57:39 +0800

    ccc

---
+++ src/main/java/com/example/quartz/UpdateAllStocksDailyTask.java
@@ -0,0 +1,34 @@
+package com.example.quartz;
+
+import com.example.domain.Holiday;
+import com.example.service.HolidayService;
+import com.example.service.StockService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Created by djklaf on 2017/1/6.
+ */
+@Component
+public class UpdateAllStocksDailyTask {
+    private static final Logger log = LoggerFactory.getLogger(UpdateAllStocksDailyTask.class);
+
+    @Autowired
+    StockService stockService;
+
+    @Autowired
+    HolidayService holidayService;
+
+    @Scheduled(cron = "0 0 16 * * ?")
+    public void updateAllStocksDaily(){
+        if(holidayService.isCloseday(new Date())){
+            return;
+        }
+        stockService.updateStocks();
+    }
+}
